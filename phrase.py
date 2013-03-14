2fe27cfb9d464038b6e33f56f123304676ab00ea
Sander Latour <latour@uva.nl>
2013-03-14 19:47:35 +0100

    Implemented phrase nodes

--- phrase.py
+++ phrase.py
@@ -1,5 +1,35 @@
 class PhraseNode:
-  left_child = None
-  right_child = None
-  formulaset = None
+  _type = 0
+  _left = None
+  _right = None
+  _formulaset = None
 
+  def __init__(self, formulaset):
+    self._formulaset = formulaset
+
+  def isComplex(self):
+    return self._type==0
+
+  def addLeft(self, left):
+    self._left = left
+  
+  def left(self):
+    return self._left
+  
+  def addRight(self, right):
+    self._right = right
+  
+  def right(self):
+    return self._right
+
+  def formulaset(self):
+    return self._formulaset
+  
+class ExemplarNode(Phrase):
+  _type = 1
+
+  def addString(self, string):
+    self.addleft(string)
+
+  def string(self):
+    return self.left()
