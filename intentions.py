da8d1b99a945624295669119d3e59d367569f6e3
Sander Latour <latour@uva.nl>
2013-03-15 13:52:00 +0100

    1) Added intentions file that the parent will use to know what needs to be said 2) Added observations file that the child will use to test its functions

---
+++ intentions.py
@@ -0,0 +1,3 @@
+intentions = [ \
+  FormulaSet([PropertyFormula('snake',1),RelationFormula('bite',1,2),PropertyFormula('pig',2)]), \
+]
---
+++ observations.py
@@ -0,0 +1,4 @@
+observations = [\
+  [('snake','bit','pig'),FormulaSet([PropertyFormula('snake',1),RelationFormula('bite',1,2),PropertyFormula('pig',2)])], \
+  [('snake','bit','pig'),FormulaSet([PropertyFormula('snake',1),RelationFormula('bite',2,1),PropertyFormula('pig',1)])], \
+]
