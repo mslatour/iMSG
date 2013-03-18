2e2b3201eff3ba27c41c1910fc56bb55b365afeb
Michael Cabot <michael.a.cabot@gmail.com>
2013-03-18 17:58:03 +0100

    fix bug

--- observations.py
+++ observations.py
@@ -1,8 +1,8 @@
 from formula import *
 
 observations = [\
-  [('snake',),FormulaSet([PropertyFormula('snake',1))])], \
-  [('pig',),FormulaSet([PropertyFormula('pig',1))])], \
+  [('snake',),FormulaSet([PropertyFormula('snake',1)])], \
+  [('pig',),FormulaSet([PropertyFormula('pig',1)])], \
   [('snake','bit','pig'),FormulaSet([PropertyFormula('snake',1),RelationFormula('bit',2,1),PropertyFormula('pig',1)])], \
   [('snake','bit','pig'),FormulaSet([PropertyFormula('snake',1),RelationFormula('bit',1,2),PropertyFormula('pig',2)])], \
 ]
