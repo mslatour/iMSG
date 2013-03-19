d43aa46bf2a6b1cd8056551c73773d86f8033bdc
mslatour <sanderlatour@gmail.com>
2013-03-19 17:36:09 +0100

    Added lexicon structure and methods

--- human.py
+++ human.py
@@ -6,9 +6,27 @@ Michael Cabot & Sander Latour
 """
 
 class Human:
+  _lexicon = {}
 
   def __init__(self):
     self.cost = 0
+
+  def lexicon(self):
+    return self._lexicon
+
+  def add_to_lexicon(self, meaning, string):
+    self._lexicon[meaning] = string
+
+  def verbalize(self, meaning):
+    if meaning in self._lexicon:
+      return self._lexicon[meaning]
+    try:
+      s = ""
+      for m in meaning:
+        s += self.verbalize(m)
+      return s
+    except TypeError te:
+      return ""
 
   def get_last_cost(self):
     return self.cost
