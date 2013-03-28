940ca32fa7212b05cf1ac23c16c0d430bf3cdabe
mslatour <sanderlatour@gmail.com>
2013-03-28 16:23:33 +0100

    1) Updated spaces 2) Wrote merge generator

--- pcfg.py
+++ pcfg.py
@@ -16,14 +16,14 @@ class PCFGRule:
         self.rhs = rhs
         self.amap = amap if amap != None else tuple([ArgumentMap()]*len(rhs))
         self.lhs = FormulaSet([x.apply_argument_map(self.amap[i]) \
-                for i,x in enumerate(rhs)])
+                for i, x in enumerate(rhs)])
         self.cost = cost
 
     def __eq__(self, item):
         return (self.rhs == item.rhs and self.amap == item.amap)
 
     def __hash__(self):
-        return hash((self.rhs,self.amap))
+        return hash((self.rhs, self.amap))
 
     def __str__(self):
         return "%s --> %s [%d]" % (self.lhs, self.rhs, self.cost)
@@ -44,7 +44,7 @@ class PCFGRule:
                 # Alter the rhs element
                 new_rhs[i] = rhs[i]
                 # Create a new PCFG rule
-                rule = PCFGRule(tuple(new_rhs),\
+                rule = PCFGRule(tuple(new_rhs), \
                         rule.cost+COST_SUBSTITUTION+costs[i], self.amap)
 
         # Create set of possible argument mappings
@@ -56,101 +56,96 @@ class PCFGRule:
         # Generate all argument variations
         for amap in amapset:
             cost = rule.cost + (0 if amap == self.amap else COST_AMAP)
-            pcfgs.append(PCFGRule(rhs,cost,amap))
+            pcfgs.append(PCFGRule(rhs, cost, amap))
 
         return pcfgs
 
 
 class Grammar:
-  _rules = []
+    _rules = []
 
-  def __init__(self, rules=[]):
-    self._rules = rules
+    def __init__(self, rules=[]):
+        self._rules = rules
 
-  def rules(self):
-    return self._rules
+    def rules(self):
+        return self._rules
 
-  def extended_grammar(self, rhs, rhs_costs):
-    extended_rules = {}
-    for rule in self.rules():
-      temp_rules = rule.extend(rhs, rhs_costs)
-      for temp in temp_rules:
-        if temp.cost() < extended_rules.get(temp, float('inf')):
-          extended_rules[temp] = temp.cost()
+    def extended_grammar(self, rhs, rhs_costs):
+        extended_rules = {}
+        for rule in self.rules():
+            temp_rules = rule.extend(rhs, rhs_costs)
+            for temp in temp_rules:
+                if temp.cost() < extended_rules.get(temp, float('inf')):
+                    extended_rules[temp] = temp.cost()
 
-    new_rules = self.create_rules(rhs[0], rhs[1]) # TODO always cheaper to create new rule?
-    for rule in new_rules:
-      if rule.cost() < extended_rules.get(rule, float('inf')):
-        extended_rules[rule] = rule.cost()
+        rule_gen = self.create_rule_generator(rhs, rhs_costs) 
+        for rule in rule_gen:
+            if rule.cost() < extended_rules.get(rule, float('inf')):
+                extended_rules[rule] = rule.cost()
 
-    return Grammar(extended_rules.keys())
+        return Grammar(extended_rules.keys())
 
-  def create_rules(self, left, right):
-    rules = []
-    # TODO
-    # get all argument mappings for left and right
-    # apply all combinations to union of left and right
-    # create rule and add to rules
-    # cost of new rule = left.cost() + right.cost() + COST_MERGE
-    return rules
+    def create_rule_generator(self, rhs, costs):
+        amapset = ArgumentMap.generate_amap_set(len(rhs))
+        return (PCFGRule(rhs, sum(costs)+COST_MERGE, amap) for amap in amapset)
 
-  def inverse(self):
-    inverse = {}
-    for rule in self.rules():
-      inverse.setdefault(rule.rhs(), []).append((rule.lhs(), rule.cost()))
+    def inverse(self):
+        inverse = {}
+        for rule in self.rules():
+            inverse.setdefault(rule.rhs(), []).append((rule.lhs(), rule.cost()))
 
-    return inverse
+        return inverse
 
-  def __add__(self, other):
-    if isinstance(other, Grammar):
-      return Grammar(self.rules() + other.rules())
-    else:
-      return NotImplemented
+    def __add__(self, other):
+        if isinstance(other, Grammar):
+            return Grammar(self.rules() + other.rules())
+        else:
+            return NotImplemented
 
-  def __eq__(self, other):
-    return set(self.rules()) == set(other.rules())
+    def __eq__(self, other):
+        return set(self.rules()) == set(other.rules())
 
-  def __lt__(self, other):
-    return NotImplemented
+    def __lt__(self, other):
+        return NotImplemented
 
-  def __le__(self, other):
-    return NotImplemented
+    def __le__(self, other):
+        return NotImplemented
 
-  def __ne__(self, other):
-    return not self == other
+    def __ne__(self, other):
+        return not self == other
 
-  def __gt__(self, other):
-    return NotImplemented
+    def __gt__(self, other):
+        return NotImplemented
 
-  def __ge__(self, other):
-    return NotImplemented
+    def __ge__(self, other):
+        return NotImplemented
 
-  def __getitem__(self,key):
-    if isinstance(key, slice):
-      return Grammar(self.rules()[key])
-    else:
-      return self.rules()[key]
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            return Grammar(self.rules()[key])
+        else:
+            return self.rules()[key]
   
-  def __setitem__(self, key, value):
-    self._rules[key] = value
+    def __setitem__(self, key, value):
+        self._rules[key] = value
 
-  def __iter__(self):
-    return self._rules.__iter__()
+    def __iter__(self):
+        return self._rules.__iter__()
   
-  def __reversed__(self):
-    return self._rules.__reversed__()
+    def __reversed__(self):
+        return self._rules.__reversed__()
 
-  def __contains__(self, item):
-    return self._rules.__contains__(item)
+    def __contains__(self, item):
+        return self._rules.__contains__(item)
 
-  def __len__(self):
-    return len(self._rules)
+    def __len__(self):
+        return len(self._rules)
 
-  def index(self, item):
-    return self._rules.index(item)
+    def index(self, item):
+        return self._rules.index(item)
 
-  def append(self, item):
-    self._rules.append(item)
+    def append(self, item):
+        self._rules.append(item)
 
-  def extend(self, item):
-    self._rules.extend(item)
+    def extend(self, item):
+        self._rules.extend(item)
