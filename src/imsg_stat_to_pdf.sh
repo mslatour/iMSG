0410a8534b28faab010a823b284677c373c9cf8a
mslatour <sanderlatour@gmail.com>
2013-04-03 23:44:40 +0200

    Created checks to deal with one point graphs

--- src/imsg_stat_to_pdf.sh
+++ src/imsg_stat_to_pdf.sh
@@ -2,11 +2,18 @@
 
 # visualize property/relation distribution
 OUTPUT=$(mktemp)
+
 cat debug_relation_stat.txt | sort | uniq -c | sort -rgk 1 | awk '{print $1}' > "${OUTPUT}.relation"
+if [ "$(wc -l ${OUTPUT}.relation | awk '{print $1}')" != "1" ]; then
+gnuplot -e "set terminal pdf; plot '${OUTPUT}.relation' u 1 w linespoints t 'Relation frequency'" > debug_stat_relation.pdf
+gnuplot -e "set terminal pdf; set log xy; plot '${OUTPUT}.relation' u 1 w linespoints t 'Relation log frequency'" > debug_stat_relation_log.pdf
+fi
+
 cat debug_property_stat.txt | sort | uniq -c | sort -rgk 1 | awk '{print $1}' > "${OUTPUT}.property"
-paste -d" " "${OUTPUT}.relation" "${OUTPUT}.property" > "${OUTPUT}.both"
-gnuplot -e "set terminal pdf; plot '${OUTPUT}.both' u 1 w linespoints title 'relation', '${OUTPUT}.both' u 2 w linespoints title 'property'" > debug_stat_word.pdf
-gnuplot -e "set terminal pdf; set log xy; plot '${OUTPUT}.both' u 1 w linespoints title 'relation', '${OUTPUT}.both' u 2 w linespoints title 'property'" > debug_stat_word_log.pdf
+if [ "$(wc -l ${OUTPUT}.property | awk '{print $1}')" != "1" ]; then
+    gnuplot -e "set terminal pdf; plot '${OUTPUT}.property' u 1 w linespoints t 'Property frequency'" > debug_stat_property.pdf
+    gnuplot -e "set terminal pdf; set log xy; plot '${OUTPUT}.property' u 1 w linespoints t 'Property log frequency'" > debug_stat_property_log.pdf
+fi
 
 # visualize grammar stats
 IN="debug_stat.txt"
